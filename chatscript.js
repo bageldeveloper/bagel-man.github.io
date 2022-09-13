84cc7b50a62fdc2e8c82aab199f6d7d6504b0ee8
Tom <57464840+bagel-man@users.noreply.github.com>
2022-09-13 10:03:12 -0600

    Update chatscript.js

--- chatscript.js
+++ chatscript.js
@@ -140,7 +140,8 @@ function stopScroll() {
 }
 
 function addMessageToListDOM(text, member) {
-  if(text.match(/(amongus|sus|test)/gi)){
+  const badwords = ["sus", "test", "amongus"];
+  if(badwords.some(v => text.includes(v)))){
      return;
      }
   const el = DOM.messages;
